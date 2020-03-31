e1ea424d17da88e0afe8592ba936a7b6f99c93b7
zhangke <zhangke-s@inspur.com>
2020-03-31 09:00:20 +0800

    src\main\java\com\rrpserivce\demo\RrpApplication.java

--- src/main/java/com/rrpserivce/demo/RrpApplication.java
+++ src/main/java/com/rrpserivce/demo/RrpApplication.java
@@ -6,10 +6,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class DemoApplication {
+public class RrpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RrpApplication.class, args);
     }
 
 }
